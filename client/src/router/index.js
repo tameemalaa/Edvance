e6967932ab1c7ee0e652fb392df71cad1778c6c3
tameemalaa <tameem.alaa@gmail.com>
2023-03-26 17:14:36 +0200

    feat : changed all routs to lazy loading
    - Added forgot password route
    - Removed Default Vue about view and component

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,32 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import LogIn from "../views/LogIn.vue";
-import SignUp from "../views/SignUp.vue";
-
 const routes = [
     {
         path: "/",
         name: "home",
-        component: HomeView,
-    },
-    {
-        path: "/about",
-        name: "about",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+        component: () => import("../views/HomeView.vue"),
     },
     {
         path: "/login",
         name: "LogIn",
-        component: LogIn,
+        component: () => import("../views/LogIn.vue"),
     },
     {
         path: "/signup",
         name: "Signup",
-        component: SignUp,
+        component: () => import("../views/SignUp.vue"),
+    },
+    {
+        path: "/forgot-password",
+        name: "ForgotPassword",
+        component: () => import("../views/ForgotPassword.vue"),
     },
 ];
 
