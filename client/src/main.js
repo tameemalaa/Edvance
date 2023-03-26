20a3dbaae58d3073e5e49daba56c70b4ba522fcd
tameemalaa <tameem.alaa@gmail.com>
2023-03-26 02:04:07 +0200

    edited home page for better dev exp

--- client/src/main.js
+++ client/src/main.js
@@ -7,7 +7,7 @@ import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
 
 import axios from "axios";
-axios.defaults.baseURL = "http://127.0.0.1:8000";
+axios.defaults.baseURL = "http://localhost:8000";
 
 createApp(App).use(Antd).use(router,axios).use(store).mount("#app");
 
