839fde44883ed126eac99e4c4ea8fbfe4b71ca0d
AI091 <ahmedibrahem00091@gmail.com>
2023-08-28 00:01:27 +0300

    [front] added dummy sidebar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <!-- <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link to="/login">LogIn</router-link> |
-        <router-link to="/signup">SignUp</router-link>
-    </nav> -->
   <router-view />
 </template>
 
---
+++ client/src/views/LandingPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <SideBar />
+  
+</template>
+
+<script>
+import SideBar from '@/components/SideBar.vue'
+
+export default {
+  components: { SideBar },
+
+}
+</script>
+
